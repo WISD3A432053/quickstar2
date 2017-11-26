6536890db1a779a837fa1d90427cb9b8e6413ebc
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-26 16:53:38 +0800

    在 app/Http/routes.php 檔案中建置一些對應至此控制器的路由：

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::auth();
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
