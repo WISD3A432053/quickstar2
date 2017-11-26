9a809facd215732aeebd6fb3a687152d30af3409
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-26 17:49:25 +0800

    連接 Task 模型與 TaskPolicy。我們可以透過增加一行至  app/Providers/AuthServiceProvider.php 檔案的 $policies 屬性做到這件事(錯誤修正)

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Policies\TaskPolicy;
+use App\Task;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Pol icies\ModelPolicy',
         Task::class => TaskPolicy::class,
     ];
 
