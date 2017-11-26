065f67ba87c2ea401f4ee781a704d2e5b571bcca
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-26 17:24:32 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Task;
+
 class TaskController extends Controller
 {
     public function __construct()
@@ -13,7 +15,11 @@ class TaskController extends Controller
 
     public function index(Request $request)
     {
-        return view('tasks.index');
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
 
     public function store(Request $request)
